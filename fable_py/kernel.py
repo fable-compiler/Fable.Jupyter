81f01cbd3452b600ba96386c9fedbc413589aa4b
Dag Brattli <dag@brattli.net>
2021-04-17 09:14:31 +0200

    Better splitting of statements / expressions

--- fable_py/kernel.py
+++ fable_py/kernel.py
@@ -51,15 +51,15 @@ class Fable(MetaKernel):
         "name": "fable-python",
     }
 
-    # For splitting code blocks into statements
+    # For splitting code blocks into statements (lines that start with identifiers or [)
     stmt_regexp = r"\n(?=[\w\[])"
     # For parsing a declaration (let, type, open) statement
     decl_regex = (
-        r"^(let)\s+(?P<let>\w+)"
-        r"|^(let)\s+``(?P<ticked>[\w ]+)``"
-        r"|^(type)\s+(?P<type>\w*)[\s\(]"
-        r"|^(open)\s+(?P<open>[\w.]+)"
-        r"|^\[<(?P<attr>.*)>\]"
+        r"^(let)\s+(?P<let>\w+)"  # e.g: let a = 10
+        r"|^(let)\s+``(?P<ticked>[\w ]+)``"  # e.g: let ``end`` = "near"
+        r"|^(type)\s+(?P<type>\w*)[\s\(]"  # e.g: type Test () class end
+        r"|^(open)\s+(?P<open>[\w.]+)"  # e.g: open Fable.Core
+        r"|^\[<(?P<attr>.*)>\]"  # e.g: [<Import(..)>]
     )
     # decl_regex = r"^(let)\s(\w*)|^(type)\s(\w*)\s*=|^(open)\s(\w*)\s"
     pyfile = "src/fable.py"
@@ -96,72 +96,69 @@ class Fable(MetaKernel):
         """Execute the code, and return result."""
         # print("code: ", code)
         self.result = None
-        ef = None
 
         # try to parse it:
         try:
-            open(self.fsfile, "w+").close()  # Clear previous errors
-            ef = open(self.erfile, "r")
-            ef.seek(0, io.SEEK_END)
-            mtime = os.path.getmtime(self.fsfile)
+            with open(self.erfile, "r") as ef:
+                ef.seek(0, io.SEEK_END)
 
-            expr = []
-            decls = []
+                with open(self.fsfile, "w+"):  # Clear previous errors
+                    pass
+                mtime = os.path.getmtime(self.fsfile)
 
-            # Remove program declarations redefined in submitted code
-            stmts = [stmt.lstrip("\n").rstrip() for stmt in re.split(self.stmt_regexp, code, re.M) if stmt]
-            print("Stmts: ", stmts)
-            for stmt in stmts:
-                match = re.match(self.decl_regex, stmt)
-                if match:
-                    matches = dict((key, value) for (key, value) in match.groupdict().items() if value)
-                    key = f"{list(matches.keys())[0]} {list(matches.values())[0]}"
-                    # print("program: ", self.program)
-                    # print("key: ", key)
-                    self.program[key] = stmt
-                    decls.append((key, stmt))
+                expr = []
+                decls = []
 
-                # We need to print single expressions (except for those printing themselves)
+                # Update program declarations redefined in submitted code
+                stmts = [stmt.lstrip("\n").rstrip() for stmt in re.split(self.stmt_regexp, code, re.M) if stmt]
+                # print("Stmts: ", stmts)
+                for stmt in stmts:
+                    match = re.match(self.decl_regex, stmt)
+                    if match:
+                        matches = dict((key, value) for (key, value) in match.groupdict().items() if value)
+                        key = f"{list(matches.keys())[0]} {list(matches.values())[0]}"
+                        # print("program: ", self.program)
+                        # print("key: ", key)
+                        self.program[key] = stmt
+                        decls.append((key, stmt))
+
+                    # We need to print single expressions (except for those printing themselves)
+                    else:
+                        expr.append(stmt)
+
+                # Construct the F# program (current and past declarations)
+                program = [stmt for stmt in self.program.values()]
+
+                # Print the result of a single expression. # TODO: can we simplify?
+                if len(expr) == 1 and "printf" not in expr[0] and not decls:
+                    expr = [f"""printfn "%A" ({expr[0]})"""]
+
+                # Write the F# program to file
+                with open(self.fsfile, "w") as f:
+                    f.write("\n".join(program))
+                    f.write("\n")
+                    f.write("\n".join(expr))
+
+                # Wait for Python file to be compiled
+                for i in range(20):
+                    # Check for compile errors
+                    if os.path.getmtime(self.erfile) > mtime:
+                        result = ef.read()
+                        self.Error(result)
+                        self.result = ""
+                        break
+
+                    # Detect if the Python file have changed.
+                    if os.path.getmtime(self.pyfile) > mtime:
+                        with open(self.pyfile, "r") as f:
+                            pycode = f.read()
+                            exec_code = compile(pycode, self.pyfile, "exec")
+                            self.result = eval(exec_code, self.locals)
+                        break
+
+                    time.sleep(0.1)
                 else:
-                    expr.append(stmt)
-
-            # Construct the F# program
-            program = [stmt for stmt in self.program.values()]
-
-            # Print the result of a single expression.
-            if len(expr) == 1 and "printf" not in expr[0] and not decls:
-                expr = [f"""printfn "%A" ({expr[0]})"""]
-
-            # Write the F# program to file
-            with open(self.fsfile, "w") as f:
-                f.write("\n".join(program))
-                f.write("\n")
-                f.write("\n".join(expr))
-
-            # Wait for Python file to be compiled
-            for i in range(20):
-                # Check for compile errors
-                if os.path.getmtime(self.erfile) > mtime:
-                    result = ef.read()
-                    self.Error(result)
-                    self.result = ""
-                    break
-
-                # Detect if the Python file have changed.
-                if os.path.getmtime(self.pyfile) > mtime:
-                    with open(self.pyfile, "r") as f:
-                        pycode = f.read()
-                        exec_code = compile(pycode, self.pyfile, "exec")
-                        self.result = eval(exec_code, self.locals)
-                    break
-
-                time.sleep(0.1)
-            else:
-                self.result = "timeout: %s : %s" % (mtime, os.path.getmtime(self.pyfile))
-
-            # Update program
-            # for key, stmt in decls:
-            #    self.program[key] = stmt
+                    self.result = "timeout: %s : %s" % (mtime, os.path.getmtime(self.pyfile))
 
         except Exception as e:
             self.Error(traceback.format_exc())
@@ -174,14 +171,11 @@ class Fable(MetaKernel):
                 }
             )
             return None
-        finally:
-            if ef is not None:
-                ef.close()
         return self.result
 
     def get_completions(self, info):
-        txt = info["help_obj"]
+        # txt = info["help_obj"]
 
-        matches = self.complete(txt)
+        matches = []  # self.complete(txt)
 
         return matches
