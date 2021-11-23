4cc6c2804add0512caef2ba6a46c8b6aa58e25d0
Dag Brattli <dag@brattli.net>
2021-11-23 18:25:04 +0100

    Fix versioning and setup

--- setup.py
+++ setup.py
@@ -2,20 +2,24 @@ import io
 
 from setuptools import find_packages, setup
 
+__version__ = None
 with io.open("fable_py/version.py", encoding="utf-8") as fid:
     for line in fid:
         if line.startswith("__version__"):
             __version__ = line.strip().split()[-1][1:-1]
             break
+assert __version__, "__version__ not set"
 
 with open("README.md") as f:
     readme = f.read()
+
 
 setup(
     name="fable_py",
     version=__version__,
     description="A Fable (python) kernel for Jupyter",
     long_description=readme,
+    long_description_content_type="text/markdown",
     author="Dag Brattli",
     author_email="dag@brattli.net",
     url="https://github.com/dbrattli/Fable.Jupyter",
@@ -27,7 +31,7 @@ setup(
         "Framework :: IPython",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: FSharp",
+        "Programming Language :: F#",
         "Topic :: System :: Shells",
     ],
 )
